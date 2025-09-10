c77fddff8b6d51adf653e7c34f6b7825164c6589
nirajan252006 <niru8904760125@gmail.com>
2025-09-10 20:59:35 +0530

    Added h1 file

---
+++ h1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    
+  printf("Hello World! \n I am learning C. \n And it is awesome! \n");
+  printf(" \n \n  \t  \\ \" Hello World!  I am learning C.  And it is \"  awesome! \n");
+  int myNum = 15;              // Integer (whole number)
+  float myFloatNum = 5.99;     // Floating point number
+  char myLetter = 'D';         // Character
+  
+  // Print variables
+  printf("%d\n", myNum);
+  printf("%f\n", myFloatNum);
+  printf("%c\n", myLetter);
+
+  int myNumm = 15;
+printf("My favorite number is: %d and my letter is   %c", myNumm ,myLetter);
+
+
+printf("\nMy favorite number is: %d", 25);
+
+  return 0;
+}
