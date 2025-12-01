d0ca2313a65da1273e4d2f2b94c60d5d5f0c3fde
nirajan252006 <niru8904760125@gmail.com>
2025-12-01 09:45:09 +0530

    Test Jenkins auto build

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ package lab1.src.main.java;
 
 public class Main {
      public static void main (String args[]){
-        System.out.println("Hello Jenkins Test Build!");
+        System.out.println("Hello from Jenkins build v2");
+
      }
     
 }
